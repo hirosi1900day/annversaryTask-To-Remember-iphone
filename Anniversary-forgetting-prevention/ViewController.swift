0414321a62e3162311cd69c0f742205964da6c04
hirosi1900daypractice <hirosi1900daypractice>
2021-04-25 19:09:04 +0900

    Initial Commit

---
+++ Anniversary-forgetting-prevention/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anniversary-forgetting-prevention
+//
+//  Created by 徳富博 on 2021/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
